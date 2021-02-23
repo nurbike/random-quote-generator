848f4fe75645db521f31b9f045f4ced6092176b9
Nurbike Aytar <nurbike0@gmail.com>
2021-02-23 20:06:21 +0300

    getting data from remote source (js)

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var quotes = ["Be yourself; everyone else is already taken. ― Oscar Wilde", "I\'m selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can\'t handle me at my worst, then you sure as hell don\'t deserve me at my best.― Marilyn Monroe", "Two things are infinite: the universe and human stupidity; and I\'m not sure about the universe.― Albert Einstein", "So many books, so little time.― Frank Zappa", "A room without books is like a body without a soul.― Marcus Tullius Cicero", "Be who you are and say what you feel, because those who mind don\'t matter, and those who matter don\'t mind.― Bernard M. Baruch", "You know you\'re in love when you can\'t fall asleep because reality is finally better than your dreams.― Dr. Seuss", "If you want to know what a man\'s like, take a good look at how he treats his inferiors, not his equals.― J.K. Rowling, Harry Potter and the Goblet of Fire", "I\'ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.― Maya Angelou", "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.― Martin Luther King Jr., A Testament of Hope: The Essential Writings and Speeches"];
-
+var quotes = [];
 
 function get() {
-var random = Math.floor(Math.random() * (quotes.length));
-random = document.getElementById("area").innerHTML;
+    var randomQuotes = Math.floor(Math.random() * (quotes.length));
+    var quote = quotes[randomQuotes].text + " - " + quotes[randomQuotes].author
+    document.getElementById("area").innerText = quote;
 }
+
+fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    quotes = data;
+    get();
+  });
+
 
